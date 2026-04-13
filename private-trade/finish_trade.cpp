820de3c999cc35fdd5031ac6d3468b35380d1b84
youngsan-sani <saniland@gmail.com>
2026-04-13 13:38:22 +0900

    Update finish_trade.cpp
    rollback 로직 반영

--- private-trade/finish_trade.cpp
+++ private-trade/finish_trade.cpp
@@ -1,27 +1,29 @@
 void CTradeDirector::finishTrade(CPlayer* player, CPlayer* targetPlayer)
 {
+	rollbackFunctionStack.clear();
+
 	// 양측의 거래 목록 아이템 삭제, 대상의 아이템이 들어올 수 있는지는 삭제 후 검사해야하므로 먼저 삭제하고 실패 시 롤백
 	auto [tradeItemList, targetTradeItemList]= getTradeItemList(player, targetPlayer);
 	if (deleteItemList(player, tradeItemList) != SUCCESS)
 	{
+		rollbackFunctionStack.rollback();
 		onFinishTrade(player, targetPlayer, TRADE_LIST_DELETE_FAIL);
 		return;
 	}
+	rollbackFunctionStack.push([=]() { rollbackDeleteItemList(player, tradeItemList); });
 
 	if (deleteItemList(targetPlayer, targetTradeItemList) != SUCCESS)
 	{
-		// 요청자의 아이템이 먼저 차감되었으므로 롤백
-		rollbackDeleteItemList(player, tradeItemList);
-
+		rollbackFunctionStack.rollback();
 		onFinishTrade(player, targetPlayer, TRADE_LIST_DELETE_FAIL);
 		return;
 	}
+	rollbackFunctionStack.push([=]() { rollbackDeleteItemList(targetPlayer, targetTradeItemList); });
 
 	// 대상의 거래 목록이 요청자의 인벤토리에 들어올 수 있는지 확인
 	if (canAddNewItemList(player, targetTradeItemList) != SUCCESS)
 	{
-		rollbackDeleteItemList(player, tradeItemList);
-		rollbackDeleteItemList(targetPlayer, targetTradeItemList);
+		rollbackFunctionStack.rollback();
 		onFinishTrade(player, targetPlayer, INVENTORY_FULL);
 		return;
 	}
@@ -29,8 +31,7 @@ void CTradeDirector::finishTrade(CPlayer* player, CPlayer* targetPlayer)
 	// 내 거래 목록이 대상의 인벤토리에 들어갈 수 있는지 확인
 	if (canAddNewItemList(targetPlayer, tradeItemList) != CS_ERROR_NONE)
 	{
-		rollbackDeleteItemList(player, tradeItemList);
-		rollbackDeleteItemList(targetPlayer, targetTradeItemList);
+		rollbackFunctionStack.rollback();
 		onFinishTrade(player, targetPlayer, TARGET_INVENTORY_FULL);
 		return;
 	}
@@ -39,51 +40,42 @@ void CTradeDirector::finishTrade(CPlayer* player, CPlayer* targetPlayer)
 	auto [tradeGold, targetTradeGold] = getTradeGold(player, targetPlayer);
 	if (useGold(player, tradeGold) != SUCCESS)
 	{
-		rollbackDeleteItemList(player, tradeItemList);
-		rollbackDeleteItemList(targetPlayer, targetTradeItemList);
+		rollbackFunctionStack.rollback();
 		onFinishTrade(player, targetPlayer, NOT_ENOUGH_GOLD);
 		return;
 	}
+	rollbackFunctionStack.push([=]() { rollbackUseGold(player, tradeGold); });
 
 	if (useGold(targetPlayer, targetTradeGold) != SUCCESS)
 	{
-		// 요청자의 골드가 먼저 차감되었으므로 롤백
-		rollbackUseGold(player, tradeGold);
-
-		rollbackDeleteItemList(player, tradeItemList);
-		rollbackDeleteItemList(targetPlayer, targetTradeItemList);
+		rollbackFunctionStack.rollback();
 		onFinishTrade(player, targetPlayer, TARGET_NOT_ENOUGH_GOLD);
 		return;
 	}
+	rollbackFunctionStack.push([=]() { rollbackUseGold(targetPlayer, targetTradeGold); });
 
 	// 거래 목록을 각각의 인벤토리에 추가
 	if (addItemList(player, targetTradeItemList) != SUCCESS)
 	{
-		rollbackUseGold(player, tradeGold);
-		rollbackUseGold(targetPlayer, targetTradeGold);
-		rollbackDeleteItemList(player, tradeItemList);
-		rollbackDeleteItemList(targetPlayer, targetTradeItemList);
+		rollbackFunctionStack.rollback();
 		onFinishTrade(player, targetPlayer, TRADE_LIST_ADD_FAIL);
 		return;
 	}
+	rollbackFunctionStack.push([=]() { rollbackAddItemList(player, targetTradeItemList); });
 
 	if (addItemList(targetPlayer, tradeItemList) != SUCCESS)
 	{
-		// 요청자의 인벤토리에 추가된 아이템 삭제
-		rollbackAddItemList(player, targetTradeItemList);
-
-		rollbackUseGold(player, tradeGold);
-		rollbackUseGold(targetPlayer, targetTradeGold);
-		rollbackDeleteItemList(player, tradeItemList);
-		rollbackDeleteItemList(targetPlayer, targetTradeItemList);
+		rollbackFunctionStack.rollback();
 		onFinishTrade(player, targetPlayer, TRADE_LIST_ADD_FAIL);
 		return;
 	}
+	rollbackFunctionStack.push([=]() { rollbackAddItemList(targetPlayer, tradeItemList); });
 
 	// 골드 추가는 항상 성공
 	addGold(player, targetTradeGold);
 	addGold(targetPlayer, tradeGold);
 
 	// 거래 성공 처리 및 거래 상태 초기화
+	rollbackFunctionStack.clear();
 	onFinishTrade(player, targetPlayer, SUCCESS);
 }
